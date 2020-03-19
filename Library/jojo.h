0bdff3bb604509b2da5f4385c11ee57d79543067
jojo69 <jojo00231.jj@gmail.com>
2020-03-19 07:02:42 +0700

    2

---
+++ Library/jojo.h
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n=4;
+    vector<string> x(n);
+    for(int i=0; i<n; i++){
+        cout << "testcin : " ; 
+        string nn;
+        cin >> nn ;
+        x[i] = nn ;
+    }
+    
+    string b = ("bra bra bra") ;
+    string c = ("information") ;
+    for(int i=0; i<n; i++){
+        cout << "[" << i+1 << "] " << x[i] << "\t" << b  << "\n" ;
+    }
+
+    while(true){
+        cout << "select : " ;
+        int i;
+        cin >> i ;
+        if(i==0)break;
+        cout << "--------------------------------------------------\n" ;
+        cout << x[i-1] << " : " << b << " " << c << "\n" ;
+        cout << "--------------------------------------------------\n" ;
+    }
+    
+    return 0;
+}
