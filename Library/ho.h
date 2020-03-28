9dc1f12564bf40f870cfa800a8e0db6c4a76c421
mekkkwiz <themakza0021@gmail.com>
2020-03-28 23:05:32 +0700

    new ho.h

--- Library/ho.h
+++ Library/ho.h
@@ -61,57 +61,7 @@ void Select_case(int num_case){
     }
 
 }
-// fix phird
-string EncryptionPASS(string y){
-    char encrypPass[69];
 
-    strcpy(encrypPass,y.c_str());
-    for (int j = 0; j < 69 && encrypPass[j] != '\0' ; j++)
-    {
-        encrypPass[j]=encrypPass[j]+3;
-    }
-    string p = encrypPass;
-    //cout <<" Pass DAFUQ :: " << p << " ";
-return p;
-}
-
-string DecrypttionID(string x){
-    char decrypID[69];
-
-    strcpy(decrypID,x.c_str());
-    for (int i = 0; i < 69 && decrypID[i] != '\0' ; i++)
-    {
-        decrypID[i]=decrypID[i]-2;
-    }
-    string deID = decrypID;
-return deID;
-}
-
-//from phird.h
-string EncryptionID(string x){
-    char encryp[69];
-
-    strcpy(encryp,x.c_str());
-    for (int i = 0; i < 69 && encryp[i] != '\0' ; i++)
-    {
-        encryp[i]=encryp[i]+2;
-    }
-    string a = encryp;
-    //cout <<"WHAT DAFUQ :: " << a << " ";
-return a;
-
-}
-string DecrypttionPASS(string x){
-    char decrypPASS[69];
-
-    strcpy(decrypPASS,x.c_str());
-    for (int i = 0; i < 69 && decrypPASS[i] != '\0' ; i++)
-    {
-        decrypPASS[i]=decrypPASS[i]-3;
-    }
-    string dePASS = decrypPASS;
-return dePASS;
-}
 //from mek.h
 bool charchecker(string text_input)
 // this function used to detect input on username must be only a-z,A-Z,0-9
@@ -186,9 +136,9 @@ bool checkduplicateID(string ID)
         
 
         
-    }
-    return false;
+    } 
     file_in.close(); 
+    return false;
 }
 
 
@@ -395,3 +345,54 @@ void selectPage(){
     }while(true);
 }
 
+//from phird.h
+string EncryptionID(string x){
+    char encryp[69];
+
+    strcpy(encryp,x.c_str());
+    for (int i = 0; i < 69 && encryp[i] != '\0' ; i++)
+    {
+        encryp[i]=encryp[i]+2;
+    }
+    string a = encryp;
+    //cout <<"WHAT DAFUQ :: " << a << " ";
+return a;
+
+}
+
+string EncryptionPASS(string y){
+    char encrypPass[69];
+
+    strcpy(encrypPass,y.c_str());
+    for (int j = 0; j < 69 && encrypPass[j] != '\0' ; j++)
+    {
+        encrypPass[j]=encrypPass[j]+3;
+    }
+    string p = encrypPass;
+    //cout <<" Pass DAFUQ :: " << p << " ";
+return p;
+}
+
+string DecrypttionID(string x){
+    char decrypID[69];
+
+    strcpy(decrypID,x.c_str());
+    for (int i = 0; i < 69 && decrypID[i] != '\0' ; i++)
+    {
+        decrypID[i]=decrypID[i]-2;
+    }
+    string deID = decrypID;
+return deID;
+}
+
+string DecrypttionPASS(string x){
+    char decrypPASS[69];
+
+    strcpy(decrypPASS,x.c_str());
+    for (int i = 0; i < 69 && decrypPASS[i] != '\0' ; i++)
+    {
+        decrypPASS[i]=decrypPASS[i]-3;
+    }
+    string dePASS = decrypPASS;
+return dePASS;
+}
